a52e314efa550bebfff95d9edb360aa8caa613c5
allinamin0120 <allinamin0120@gmail.com>
2026-04-01 08:16:41 +0900

    Update seriesSP.js

--- data/seriesSP.js
+++ data/seriesSP.js
@@ -2,16 +2,16 @@ export const cardsSP = [
   // ===== SP =====
   { id: "SP-001", name: "おねがいアイプリセーブカード", char: "", rarity: "スペシャル", series: "SP", img: "images/img_e2603191-5.jpg", how: "2026年4/4（土）アイプリ設置店で配布（小学生以下）" },
 
-  { id: "SP-002", name: "プリズムハートピンク", char: "いのり", rarity: "スペシャル", series: "SP", img: "images/OTOY-000_O.webp", how: "" },
-  { id: "SP-003", name: "プリズムハートブルー", char: "あおい", rarity: "スペシャル", series: "SP", img: "images/OTOY-001_O.webp", how: "" },
+  { id: "SP-002", name: "プリズムハートピンク", char: "いのり", rarity: "スペシャル", series: "SP", img: "images/OTOY-000_O.webp", how: "2026年4/2発売おねがいアイプリ ミラーパクト プリズムピンク同封" },
+  { id: "SP-003", name: "プリズムハートブルー", char: "あおい", rarity: "スペシャル", series: "SP", img: "images/OTOY-001_O.webp", how: "2026年4/2発売おねがいアイプリ ミラーパクト プリズムブルー同封" },
 
-  { id: "SP-004", name: "アイプリすきすきレッド", char: "いのり", rarity: "スペシャル", series: "SP", img: "images/OTOY-002_O.webp", how: "" },
-  { id: "SP-005", name: "アイプリゆめゆめブルー", char: "あおい", rarity: "スペシャル", series: "SP", img: "images/OTOY-003_O.webp", how: "" },
+  { id: "SP-004", name: "アイプリすきすきレッド", char: "いのり", rarity: "スペシャル", series: "SP", img: "images/OTOY-002_O.webp", how: "2026年4/2発売おねがいアイプリデビューカードセット" },
+  { id: "SP-005", name: "アイプリゆめゆめブルー", char: "あおい", rarity: "スペシャル", series: "SP", img: "images/OTOY-003_O.webp", how: "2026年4/2発売おねがいアイプリデビューカードセット" },
 
-  { id: "SP-006", name: "リボンアイドルパステル", char: "いのり", rarity: "スペシャル", series: "SP", img: "images/OTOY-004_O.webp", how: "" },
-  { id: "SP-007", name: "リボンアイドルパステル", char: "あおい", rarity: "スペシャル", series: "SP", img: "images/OTOY-005_O.webp", how: "" },
+  { id: "SP-006", name: "リボンアイドルパステル", char: "いのり", rarity: "スペシャル", series: "SP", img: "images/OTOY-004_O.webp", how: "2026年4/2発売おねがいアイプリ ファイルバッグセット同封" },
+  { id: "SP-007", name: "リボンアイドルパステル", char: "あおい", rarity: "スペシャル", series: "SP", img: "images/OTOY-005_O.webp", how: "2026年4/2発売おねがいアイプリ ファイルバッグセット同封" },
 
-  { id: "SP-008", name: "プリズムハートパープル", char: "あおい", rarity: "スペシャル", series: "SP", img: "images/OTOY-015_O.webp", how: "" },
+  { id: "SP-008", name: "プリズムハートパープル", char: "あおい", rarity: "スペシャル", series: "SP", img: "images/OTOY-015_O.webp", how: "4/2～おねがいアイプリ ミラーパクトご購入キャンペーン" },
 
   { id: "SP-009", name: "すきのクリスタルいのりピンク", char: "いのり", rarity: "スペシャル", series: "SP", img: "images/OP-014_O.webp", how: "2026春頃～イベント配布" },
 
