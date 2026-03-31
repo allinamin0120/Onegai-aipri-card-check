373fd5ee511f7ed464991ad5a8b17850475240e7
allinamin0120 <allinamin0120@gmail.com>
2026-04-01 02:28:27 +0900

    Update seriesSP.js

--- data/seriesSP.js
+++ data/seriesSP.js
@@ -1,1 +1,20 @@
 export const cardsSP = [
+  // ===== SP =====
+{ id: "SP-001", name: "おねがいアイプリセーブカード", char: "", rarity: "スペシャル", series: "SP", img: "images/img_e2603191-5.jpg" },
+
+{ id: "SP-002", name: "プリズムハートピンク", char: "いのり", rarity: "スペシャル", series: "SP", img: "images/OTOY-000_O.webp" },
+{ id: "SP-003", name: "プリズムハートブルー", char: "あおい", rarity: "スペシャル", series: "SP", img: "images/OTOY-001_O.webp" },
+
+{ id: "SP-004", name: "アイプリすきすきレッド", char: "いのり", rarity: "スペシャル", series: "SP", img: "images/OTOY-002_O.webp" },
+{ id: "SP-005", name: "アイプリゆめゆめブルー", char: "あおい", rarity: "スペシャル", series: "SP", img: "images/OTOY-003_O.webp" },
+
+{ id: "SP-006", name: "リボンアイドルパステル", char: "いのり", rarity: "スペシャル", series: "SP", img: "images/OTOY-004_O.webp" },
+{ id: "SP-007", name: "リボンアイドルパステル", char: "あおい", rarity: "スペシャル", series: "SP", img: "images/OTOY-005_O.webp" },
+
+{ id: "SP-008", name: "プリズムハートパープル", char: "あおい", rarity: "スペシャル", series: "SP", img: "images/OTOY-015_O.webp" },
+
+{ id: "SP-009", name: "すきのクリスタルいのりピンク", char: "いのり", rarity: "スペシャル", series: "SP", img: "images/OP-014_O.webp" },
+
+{ id: "SP-010", name: "ほしのゆうびんやさんイエロー", char: "あおい", rarity: "スペシャル", series: "SP", img: "images/img_e2603191-3.jpg" },
+
+{ id: "SP-011", name: "すきすきハートバズリウム", char: "いのり", rarity: "スペシャル", series: "SP", img: "images/img_e2603191-2.jpg" },
