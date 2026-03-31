8556f4a874531ae0084a329501efaf63bb7a6dc4
allinamin0120 <allinamin0120@gmail.com>
2026-04-01 06:47:43 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 // main.js
+async function loadParts() {
+  const load = async (id, path) => {
+    const res = await fetch(path);
+    const html = await res.text();
+    document.getElementById(id).innerHTML = html;
+  };
+
+  await load("headerArea", "../parts/header.html");
+  await load("menuArea", "../parts/menu.html");
+  await load("overlayArea", "../parts/overlay.html");
+  await load("filterArea", "../parts/filter.html");
+  await load("updateArea", "../parts/update.html");
+}
 
 import { render } from './render.js';
 import { setupUI } from './ui.js';
