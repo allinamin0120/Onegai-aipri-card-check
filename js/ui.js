5396b70e5d314924d86bb47a7a7133563291e41e
allinamin0120 <allinamin0120@gmail.com>
2026-04-01 06:31:02 +0900

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,79 @@
+// ui.js
 
+import { render } from './render.js';
+import { exportData, importData } from './storage.js';
+import { cards } from './render.js';
+
+export function setupUI() {
+  const menu = document.getElementById("sideMenu");
+  const app = document.getElementById("app");
+  const overlay = document.getElementById("overlay");
+
+  window.toggleMenu = () => {
+    menu.classList.toggle("open");
+    app.classList.toggle("shift");
+    overlay.classList.toggle("active");
+  };
+
+  window.closeMenu = () => {
+    menu.classList.remove("open");
+    app.classList.remove("shift");
+    overlay.classList.remove("active");
+  };
+
+  window.exportData = () => exportData(cards);
+  window.importData = importData;
+
+  window.toggleFilter = (type) => {
+    const unowned = document.getElementById("onlyUnowned");
+    const ownedCheck = document.getElementById("onlyOwned");
+
+    if (type === "unowned" && unowned.checked) {
+      ownedCheck.checked = false;
+    }
+
+    if (type === "owned" && ownedCheck.checked) {
+      unowned.checked = false;
+    }
+
+    render();
+  };
+
+  overlay.addEventListener("click", window.closeMenu);
+
+  // スワイプ
+  let startX = 0;
+  let currentX = 0;
+  let isTouching = false;
+
+  document.addEventListener("touchstart", (e) => {
+    startX = e.touches[0].clientX;
+    const isOpen = menu.classList.contains("open");
+    if (startX < 50 || isOpen) isTouching = true;
+  });
+
+  document.addEventListener("touchmove", (e) => {
+    if (!isTouching) return;
+    currentX = e.touches[0].clientX;
+  });
+
+  document.addEventListener("touchend", () => {
+    if (!isTouching) return;
+
+    const diff = currentX - startX;
+
+    if (diff > 80) {
+      menu.classList.add("open");
+      app.classList.add("shift");
+      overlay.classList.add("active");
+    }
+
+    if (diff < -80) {
+      menu.classList.remove("open");
+      app.classList.remove("shift");
+      overlay.classList.remove("active");
+    }
+
+    isTouching = false;
+  });
+}
