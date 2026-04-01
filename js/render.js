7d4e24216b26c919dd6bded7eff21057016b455e
allinamin0120 <allinamin0120@gmail.com>
2026-04-01 11:16:27 +0900

    Update render.js

--- js/render.js
+++ js/render.js
@@ -3,7 +3,7 @@ export const cards = [];
 
 import { getOwned, isOwned, setOwned } from './storage.js';
 
-let displayCount = 100;
+let displayCount = 20;
 
 function getRarityValue(card) {
   const r = String(card.rarity || "");
